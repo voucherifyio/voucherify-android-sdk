8431466b7defb80b500c2fa84a1f72b77d9762b5
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2016-05-18 14:31:45 +0200

    voucher checkout component - javadoc

--- library/src/main/java/pl/rspective/voucherify/android/view/VoucherCheckoutView.java
+++ library/src/main/java/pl/rspective/voucherify/android/view/VoucherCheckoutView.java
@@ -15,7 +15,10 @@ import pl.rspective.voucherify.android.client.model.VoucherResponse;
 import retrofit.RetrofitError;
 
 /**
- * TODO: document your custom view class.
+ * VoucherCheckoutView is a UI component that makes it easier to validate discount codes in
+ * <a href="http://voucherify.io">Voucherify</a>. The view consists of a text field to input
+ * voucher code and a button to send the code for validation. You can get the validation result by
+ * attaching an @{link OnValidatedListener}.
  */
 public class VoucherCheckoutView extends LinearLayout {
 
@@ -88,10 +91,20 @@ public class VoucherCheckoutView extends LinearLayout {
 
     }
 
+    /**
+     * Sets {@link VoucherifyAndroidClient} - required for making validation requests to the Voucherify API.
+     *
+     * @param voucherifyClient
+     */
     public void setVoucherifyClient(VoucherifyAndroidClient voucherifyClient) {
         this.voucherifyClient = voucherifyClient;
     }
 
+    /**
+     * A listener that gets invoked when a validation result has been received.
+     *
+     * @param listener
+     */
     public void setOnValidatedListener(OnValidatedListener listener){
         this.onValidatedListener = listener;
     }
