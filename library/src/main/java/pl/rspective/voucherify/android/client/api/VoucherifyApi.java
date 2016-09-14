0ded5881d6c7cbc1f7c80f94965c2e055a5165f2
marcin.polak <mpolak87@gmail.com>
2016-09-14 22:15:20 +0200

    Add client redeem endpoint and increase grade version to 2.1.0

--- library/src/main/java/pl/rspective/voucherify/android/client/api/VoucherifyApi.java
+++ library/src/main/java/pl/rspective/voucherify/android/client/api/VoucherifyApi.java
@@ -2,9 +2,10 @@ package pl.rspective.voucherify.android.client.api;
 
 import java.util.Map;
 
-import pl.rspective.voucherify.android.client.exception.VoucherifyError;
+import pl.rspective.voucherify.android.client.model.VoucherRedemptionResult;
 import pl.rspective.voucherify.android.client.model.VoucherResponse;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Query;
 import retrofit.http.QueryMap;
 
@@ -12,5 +13,8 @@ public interface VoucherifyApi {
 
     @GET("/client/v1/validate")
     VoucherResponse validateVoucher(@QueryMap Map<String, String> queryParams);
+
+    @POST("/client/v1/redeem")
+    VoucherRedemptionResult redeemVoucher(@Query("code") String code, @Query("tracking_id") String trackingId);
 
 }
