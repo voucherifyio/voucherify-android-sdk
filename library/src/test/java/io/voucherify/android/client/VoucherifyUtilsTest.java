1be9f1fb889a00aecd306154c690705f53915954
ktalanda <ktalanda@k2net.pl>
2017-01-04 08:03:08 +0100

    make tests more unit focus

--- library/src/test/java/io/voucherify/android/client/VoucherifyUtilsTest.java
+++ library/src/test/java/io/voucherify/android/client/VoucherifyUtilsTest.java
@@ -13,37 +13,79 @@ import static org.junit.Assert.assertEquals;
 
 public class VoucherifyUtilsTest {
 
+    // calculatePrice
+    //percentage
     @Test
-    public void calculatePricePercentage() throws Exception {
+    public void calculatePrice_percentageDiscountGreaterThanZero_returnRightValue() throws Exception {
         testPricePercentage("discount > 0", 10.0, 10.0, 9.0);
+    }
+
+    @Test
+    public void calculatePrice_percentageDiscountEqualsZero_returnRightValue() throws Exception {
         testPricePercentage("discount == 0", 10.0, 0.0, 10.0);
     }
 
+    //amount
     @Test
-    public void calculatePriceAmount() throws Exception {
+    public void calculatePrice_amountDiscountLessThanPrice_returnRightValue() throws Exception {
         testPriceAmount("discount < price", 10.0, 10, 9.9);
+    }
+
+    @Test
+    public void calculatePrice_amountDiscountEqualsPrice_returnZero() throws Exception {
         testPriceAmount("discount == price", 10.0, 1000, 0.0);
+    }
+
+    @Test
+    public void calculatePrice_amountDiscountGreaterThanPrice_returnZero() throws Exception {
         testPriceAmount("discount > price", 10.0, 2000, 0.0);
+    }
+
+    @Test
+    public void calculatePrice_amountDiscountEqualsZero_returnRightValue() throws Exception {
         testPriceAmount("discount == 0", 10.0, 0, 10.0);
     }
 
+    // unit
     @Test
-    public void calculatePriceUnit() throws Exception {
+    public void calculatePrice_unitDiscountGreaterThanZero_returnRightValue() throws Exception {
         testPriceUnit("unitOff > 0", 10.0, 1.0, 2.0, 8.0);
+    }
+
+    @Test
+    public void calculatePrice_unitDiscountEqualZero_returnRightValue() throws Exception {
         testPriceUnit("unitOff == 0", 10.0, 1.0, 0.0, 10.0);
     }
 
+    // gift
     @Test
-    public void calculatePriceGift() throws Exception {
+    public void calculatePrice_giftBalanceLessThanPrice_returnRightValue() throws Exception {
         testPriceGift("giftBalance < price", 10.0, 100, 9.0);
-        testPriceGift("giftBalance == 0", 10.0, 0, 10.0);
+    }
+
+    @Test
+    public void calculatePrice_giftBalanceEqualPrice_returnZero() throws Exception {
         testPriceGift("giftBalance == price", 10.0, 10000, 0.0);
+    }
+
+    @Test
+    public void calculatePrice_giftBalanceGreaterThanPrice_returnZero() throws Exception {
         testPriceGift("giftBalance > price", 10.0, 20000, 0.0);
+    }
+
+    @Test
+    public void calculatePrice_giftBalanceEqualZero_returnRightValue() throws Exception {
+        testPriceGift("giftBalance == 0", 10.0, 0, 10.0);
+    }
+
+    @Test
+    public void calculatePrice_giftBalanceLessThanZero_returnRightValue() throws Exception {
         testPriceGift("giftBalance < 0", 10.0, -100, 11.0);
     }
 
+    // exception
     @Test(expected = RuntimeException.class)
-    public void calculatePriceInvalidType() throws Exception {
+    public void calculatePrice_invalidType_throwException() throws Exception {
         BigDecimal basePrice = new BigDecimal(10);
         Discount discount = createDiscount(null, null, null, null, null);
         VoucherResponse voucherResponse = createVoucherResponse("", true, discount, null, null, null);
@@ -51,76 +93,118 @@ public class VoucherifyUtilsTest {
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePricePercentageMoreThanHundred() throws Exception {
+    public void calculatePrice_percentageMoreThanHundred_throwException() throws Exception {
         testPricePercentage("discount > 100", 10.0, 110.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePricePercentageLessThanZero() throws Exception {
+    public void calculatePrice_percentageLessThanZero_throwException() throws Exception {
         testPricePercentage("discount < 0", 10.0, -10.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePricePercentageNull() throws Exception {
+    public void calculatePrice_percentageNull_throwException() throws Exception {
         testPricePercentage("discount == null", 10.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePriceAmountLessThanZero() throws Exception {
+    public void calculatePrice_amountLessThanZero_throwException() throws Exception {
         testPriceAmount("discount < 0", 10.0, -10, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePriceAmountNull() throws Exception {
+    public void calculatePrice_amountNull_throwException() throws Exception {
         testPriceAmount("discount == null", 10.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePriceUnitLessThanZero() throws Exception {
+    public void calculatePrice_unitLessThanZero_throwException() throws Exception {
         testPriceUnit("unitOff < 0", 10.0, 1.0, -2.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePriceUnitNull() throws Exception {
+    public void calculatePrice_unitNull_throwException() throws Exception {
         testPriceUnit("unitOff == null", 10.0, 1.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculatePriceGiftNull() throws Exception {
+    public void calculatePrice_giftNull_throwException() throws Exception {
         testPriceGift("giftBalance == null", 10.0, null, 0.0);
     }
 
+    // discount
+    // percentage
     @Test
-    public void calculateDiscountPercentage() throws Exception {
+    public void calculateDiscount_percentageDiscountGreaterThanZero_returnRightValue() throws Exception {
         testDiscountPercentage("discount > 0", 10.0, 10.0, 1.0);
+    }
+
+    @Test
+    public void calculateDiscount_percentageDiscountEqualsZero_returnZero() throws Exception {
         testDiscountPercentage("discount == 0", 10.0, 0.0, 0.0);
     }
 
+    // amount
     @Test
-    public void calculateDiscountAmount() throws Exception {
+    public void calculateDiscount_amountDiscountLessThanPrice_returnRightValue() throws Exception {
         testDiscountAmount("discount < price", 10.0, 10, 0.1);
+    }
+
+    @Test
+    public void calculateDiscount_amountDiscountEqualPrice_returnRightValue() throws Exception {
         testDiscountAmount("discount == price", 10.0, 1000, 10.0);
+    }
+
+    @Test
+    public void calculateDiscount_amountDiscountGreaterThanPrice_returnRightValue() throws Exception {
         testDiscountAmount("discount > price", 10.0, 2000, 10.0);
+    }
+
+    @Test
+    public void calculateDiscount_amountDiscountEqualZero_returnZero() throws Exception {
         testDiscountAmount("discount == 0", 10.0, 0, 0.0);
     }
 
+    // unit
     @Test
-    public void calculateDiscountUnit() throws Exception {
+    public void calculateDiscount_unitDiscountGreaterThanZero_returnRightValue() throws Exception {
         testDiscountUnit("unitOff > 0", 10.0, 1.0, 2.0, 2.0);
+    }
+
+    @Test
+    public void calculateDiscount_unitDiscountEqualZero_returnZero() throws Exception {
         testDiscountUnit("unitOff == 0", 10.0, 1.0, 0.0, 0.0);
     }
 
+    // gift
     @Test
-    public void calculateDiscountGift() throws Exception {
+    public void calculateDiscount_giftBalanceLessThanPrice_returnRightValue() throws Exception {
         testDiscountGift("giftBalance < price", 10.0, 100, 1.0);
-        testDiscountGift("giftBalance == 0", 10.0, 0, 0.0);
-        testDiscountGift("giftBalance == price", 10.0, 10000, 10.0);
+    }
+
+    @Test
+    public void calculateDiscount_giftBalanceGreaterThanPrice_returnRightValue() throws Exception {
         testDiscountGift("giftBalance > price", 10.0, 20000, 10.0);
+    }
+
+    @Test
+    public void calculateDiscount_giftBalanceEqualPrice_returnRightValue() throws Exception {
+        testDiscountGift("giftBalance == price", 10.0, 10000, 10.0);
+    }
+
+    @Test
+    public void calculateDiscount_giftBalanceEqualZero_returnZero() throws Exception {
+        testDiscountGift("giftBalance == 0", 10.0, 0, 0.0);
+    }
+
+    @Test
+    public void calculateDiscount_giftBalanceLessThanZero_returnRightValue() throws Exception {
         testDiscountGift("giftBalance < 0", 10.0, -100, -1.0);
     }
 
+    // exception
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountInvalidType() throws Exception {
+    public void calculateDiscount_invalidType_throwException() throws Exception {
         BigDecimal basePrice = new BigDecimal(10);
         Discount discount = createDiscount(null, null, null, null, null);
         VoucherResponse voucherResponse = createVoucherResponse("", true, discount, null, null, null);
@@ -128,42 +212,42 @@ public class VoucherifyUtilsTest {
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountPercentageMoreThanHundred() throws Exception {
+    public void calculateDiscount_percentageMoreThanHundred_throwException() throws Exception {
         testDiscountPercentage("discount > 100", 10.0, 110.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountPercentageLessThanZero() throws Exception {
+    public void calculateDiscount_percentageLessThanZero_throwException() throws Exception {
         testPricePercentage("discount < 0", 10.0, -10.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountPercentageNull() throws Exception {
+    public void calculateDiscount_percentageNull_throwException() throws Exception {
         testDiscountPercentage("discount == null", 10.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountAmountLessThanZero() throws Exception {
+    public void calculateDiscount_amountLessThanZero_throwException() throws Exception {
         testDiscountAmount("discount < 0", 10.0, -10, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountAmountNull() throws Exception {
+    public void calculateDiscount_amountNull_throwException() throws Exception {
         testDiscountAmount("discount == null", 10.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountUnitLessThanZero() throws Exception {
+    public void calculateDiscount_unitLessThanZero_throwException() throws Exception {
         testDiscountUnit("unitOff < 0", 10.0, 1.0, -2.0, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountUnitNull() throws Exception {
+    public void calculateDiscount_unitNull_throwException() throws Exception {
         testDiscountUnit("unitOff == null", 10.0, 1.0, null, 0.0);
     }
 
     @Test(expected = RuntimeException.class)
-    public void calculateDiscountGiftNull() throws Exception {
+    public void calculateDiscount_giftNull_throwException() throws Exception {
         testDiscountGift("giftBalance == null", 10.0, null, 0.0);
     }
 
